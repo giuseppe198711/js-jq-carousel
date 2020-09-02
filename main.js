ddec7ed40644c225b1457bdb8d84f32616561f36
Giuseppe <giuseppedf8711@gmail.com>
2020-09-02 14:50:11 +0200

    1 passo...piano piano...

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  // prendo la classe ".next" (ossia l'angle destro) e applicando un click vogliamo
+  // che esegua ciò che si torva nella funzione.
+  $ (".next").click(
+    // inizio funzione
+    function() {
+      // applico una variabile chiamata "imgAct"
+      var imgAct = $("img.active");
+      // a "imgAct" rimuoviamo la classe active
+      imgAct = removeClass("active");
+
+      
+
+    }
+  )
+
+
+
+
+
+
+
+})
